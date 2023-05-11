db722633d3b5cded0cfc68e61be0cd99ed05565a
Daniel RBJ <d.r.bond.juez@hotmail.com>
2023-05-11 16:02:13 +0200

    Changes to the trajectory display. Now instead of squares the display draws a line which changes hue with a maximum value and saturation as the trayectory gains points.

--- app/src/main/java/viewController/TrajectoryViewController.java
+++ app/src/main/java/viewController/TrajectoryViewController.java
@@ -56,7 +56,7 @@ public class TrajectoryViewController {
         Bitmap currentBitmap = Bitmap.createBitmap(BITMAP_SIZE, BITMAP_SIZE, Bitmap.Config.RGB_565);
         Canvas canvas = new Canvas(currentBitmap);
 
-
+/*
         for (int i = 0; i < xCoordinates.length; i++) {
             Rect ourRect = new Rect();
             ourRect.set(xCoordinates[i],yCoordinates[i],xCoordinates[i]+SQUARE_SIZE,yCoordinates[i]+SQUARE_SIZE);
@@ -68,6 +68,22 @@ public class TrajectoryViewController {
             canvas.drawRect(ourRect,blue);
         }
 
+ */
+
+        float hueUnit = 340/xCoordinates.length;
+
+        if(xCoordinates.length > 1) {
+            for (int i = 0; i < xCoordinates.length - 1; i++) {
+
+                float[] hsv ={hueUnit*i,100,100};
+                Paint yellow = new Paint();
+                yellow.setColor(Color.HSVToColor(hsv));
+                yellow.setStrokeWidth(10);
+
+                canvas.drawLine(xCoordinates[i], yCoordinates[i], xCoordinates[i+1], yCoordinates[i+1], yellow);
+            }
+        }
+
 
         //viewController.setTrajectory(currentBitmap);
         return currentBitmap;
@@ -77,6 +93,32 @@ public class TrajectoryViewController {
         viewController.setTrajectory(currentTrajectory);
     }
      */
+
+    private static String hsvToRgb(float hue, float saturation, float value) {
+
+        int h = (int)(hue * 6);
+        float f = hue * 6 - h;
+        float p = value * (1 - saturation);
+        float q = value * (1 - f * saturation);
+        float t = value * (1 - (1 - f) * saturation);
+
+        switch (h) {
+            case 0: return rgbToString(value, t, p);
+            case 1: return rgbToString(q, value, p);
+            case 2: return rgbToString(p, value, t);
+            case 3: return rgbToString(p, q, value);
+            case 4: return rgbToString(t, p, value);
+            case 5: return rgbToString(value, p, q);
+            default: throw new RuntimeException("Something went wrong when converting from HSV to RGB. Input was " + hue + ", " + saturation + ", " + value);
+        }
+    }
+
+    private static String rgbToString(float r, float g, float b) {
+        String rs = Integer.toHexString((int)(r * 256));
+        String gs = Integer.toHexString((int)(g * 256));
+        String bs = Integer.toHexString((int)(b * 256));
+        return rs + gs + bs;
+    }
 
 
 }
