49396ac45b9ed0c91d4c2f6f89684a667a43b7ea
Daniel RBJ <d.r.bond.juez@hotmail.com>
2023-06-16 14:05:31 +0200

    RecyclerViewAdapter and RecyclerViewController have a preliminary implementation with interaction with the TrajectoryVisualizerViewController and assuming the content past to it are the string corresponding to the file names to inflate the recycler view. The idea is that checking the box on a will will trigger a series of methods that end up in the TrajectoriesVisualizerController where it will change the boolean status of a list of a new model object called trajectory.

--- app/src/main/java/viewController/RecyclerView/RecyclerViewAdapter.java
+++ app/src/main/java/viewController/RecyclerView/RecyclerViewAdapter.java
@@ -1,4 +1,94 @@
 package viewController.RecyclerView;
 
-public class RecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.locationtrayectorydrawing.R;
+
+import java.util.ArrayList;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    private final RecyclerViewController recyclerViewController;
+    private final ArrayList<String> trajectoryFileArrayList;
+
+    public RecyclerViewAdapter(ArrayList<String> trajectoryFileArrayList, RecyclerViewController recyclerViewController) {
+        this.trajectoryFileArrayList = trajectoryFileArrayList;
+        this.recyclerViewController = recyclerViewController;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.trajectory_file_list_view, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+
+        //Change the display elements of the holder
+        holder.trajectoryFileCheckBox.setText(trajectoryFileArrayList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return trajectoryFileArrayList.size();
+    }
+
+    //Nested class necessary for the Recycler Adapter
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private final CheckBox trajectoryFileCheckBox;
+
+        public MyViewHolder(final View view) {
+            super(view);
+            trajectoryFileCheckBox = view.findViewById(R.id.trajectoryFileCheckBox);
+            //titleTextView.setSelected(true); //So long title move horizontally
+
+            trajectoryFileCheckBox.setOnCheckedChangeListener(setOnTrajectoryCheckBoxChange());
+            //view.setOnClickListener(setOnClick());
+        }
+
+        private CompoundButton.OnCheckedChangeListener setOnTrajectoryCheckBoxChange() {
+            return new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    int position = getAdapterPosition();
+                    if (recyclerViewController != null) {
+                        if (isChecked) {
+                            recyclerViewController.trajectoryFileChecked(position);
+                        } else {
+                            recyclerViewController.trajectoryFileUnchecked(position);
+                        }
+                    }
+                }
+            };
+        }
+
+        /*
+        private View.OnClickListener setOnClick (){
+            return new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    if (recyclerViewController != null) {
+                        int position = getAdapterPosition();
+
+                        if (position != RecyclerView.NO_POSITION) {
+                            recyclerViewController.onSongClicked(position);
+                        }
+                    }
+                }
+            };
+        }
+
+         */
+    }
 }
--- app/src/main/java/viewController/RecyclerView/RecyclerViewController.java
+++ app/src/main/java/viewController/RecyclerView/RecyclerViewController.java
@@ -1,4 +1,62 @@
 package viewController.RecyclerView;
 
+
+import android.app.Activity;
+
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import viewController.TrajectoriesVisualizerViewController;
+
+
+/**
+ * Provides an object that encapsulates the responsibilities behind managing the recyclerView it implements
+ * RecyclerViewObservable following an observable-observer design pattern to facilitate communication with
+ * RecyclerViewObserver implementations
+ */
 public class RecyclerViewController {
+
+    private final RecyclerView trajectoryVisualizerRecyclerView;
+    private final TrajectoriesVisualizerViewController trajectoriesVisualizerViewController;
+
+    /**
+     * This is the constructor.
+     * @param
+     */
+    public RecyclerViewController(RecyclerView trajectoryVisualizerRecyclerView,TrajectoriesVisualizerViewController trajectoriesVisualizerViewController) {
+        this.trajectoryVisualizerRecyclerView = trajectoryVisualizerRecyclerView;
+        this.trajectoriesVisualizerViewController = trajectoriesVisualizerViewController;
+    }
+
+    /**
+     * Procedure required for the inflation/filling of the recyclerView using a RecyclerViewAdapter and
+     * a layoutManager
+     * @param activity Activity object corresponding to the activity that built the MainViewController and responsible for the apps execution
+     * @param trajectoryFileList ArrayList of Song object corresponding to the songs that will be accessed by the player
+     */
+    //Method to set up the intermediate class necessary to build and control the layout and behaviour of the recyclerView
+    public void setRecyclerViewAdapter(Activity activity, ArrayList<String> trajectoryFileList) {
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(trajectoryFileList, this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(activity.getApplicationContext());
+        trajectoryVisualizerRecyclerView.setLayoutManager(layoutManager);
+        trajectoryVisualizerRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        trajectoryVisualizerRecyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    public RecyclerView getTrajectoryVisualizerRecyclerViewRecyclerView() {
+        return trajectoryVisualizerRecyclerView;
+    }
+
+    //Methods related to clicking on the recyclerView
+    public void trajectoryFileChecked(int position) {
+        trajectoriesVisualizerViewController.trajectoryFileChecked(position);
+    }
+
+    public void trajectoryFileUnchecked(int position) {
+        trajectoriesVisualizerViewController.trajectoryFileUnChecked(position);
+    }
 }
